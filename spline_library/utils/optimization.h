f155408727dd54b379d9acb8969620e6748b5366
ejmahler <join.together@gmail.com>
2014-03-15 06:44:47 -0500

    added documentation for brent's method function

--- spline_library/utils/optimization.h
+++ spline_library/utils/optimization.h
@@ -6,6 +6,10 @@
 class Optimization
 {
 public:
+    //Brent's method: Find 'x' between a and b for continuous function 'f' such that f(x) = 0
+    //in other words, return a point between a and b where the given function crosses the x axis
+    //fa mus be equal to f(a), fb must be equal to f(b), fa and fb must ahve opposite signs
+    //the algorithm returns when abs(f(x)) is less than 'tolerance'
     static double brentsMethod(const std::function<double (double)> &f, double a, double fa, double b, double fb, double tolerance = .0001);
 private:
     Optimization();
